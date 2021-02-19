ec5b4666cdd804c61fe308512fdb6f03cfd3ac82
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-19 16:40:46 +0100

    Update elastic.py

--- pyiron_gpl/elastic/elastic.py
+++ pyiron_gpl/elastic/elastic.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
-# Distributed under the terms of "New BSD License", see the LICENSE file.
+# Distributed under the terms of "GPLv3", see the LICENSE file.
 
 from collections import OrderedDict
 
