dc63693512c5e8e49b0e98a143248264016ba57a
Yury Lysogorskiy <yura.lysogorskii@gmail.com>
2022-05-24 17:10:35 +0200

    remove PaceMakerJob from pyiron_gpl repo (it will be moved into pyiron_contrib)

--- pyiron_gpl/__init__.py
+++ pyiron_gpl/__init__.py
@@ -6,7 +6,6 @@ from pyiron_base import Notebook, install_dialog, JOB_CLASS_DICT
 
 # Make classes available for new pyiron version
 JOB_CLASS_DICT["ElasticMatrixJob"] = "pyiron_gpl.elastic.elastic"
-JOB_CLASS_DICT["PaceMakerJob"] = "pyiron_gpl.pacemaker.pacemaker"
 JOB_CLASS_DICT["QuickFF"] = "pyiron_gpl.quickff.quickff"
 JOB_CLASS_DICT["Yaff"] = "pyiron_gpl.yaff.yaff"
 
