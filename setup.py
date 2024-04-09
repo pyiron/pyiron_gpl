7e7eb9ee5b5f2bd270fbdee84475daa2a1f4352c
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-09 08:21:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,10 +30,7 @@ setup(
     install_requires=[
         'molmod==1.4.8',
         'numpy==1.26.4',
-        'atomistics==0.1.24',
         'pyiron_atomistics==0.5.0',
-        'spglib==2.3.1',
-        'scipy==1.12.0',
         'quickff==2.2.4',
         'tamkin==1.2.6',
         'yaff==1.6.0'
